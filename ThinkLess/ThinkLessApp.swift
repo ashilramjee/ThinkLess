d7e6167748a04cbf21be152cb336b37c5fb09246
X-Line <ashilr11@gmail.com>
2022-12-29 20:39:47 +0200

    Initial Commit

---
+++ ThinkLess/ThinkLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkLessApp.swift
+//  ThinkLess
+//
+//  Created by Ashil Ramjee on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct ThinkLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
